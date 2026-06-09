1dec436dfea0737a747847f9181b93a04f2c37ba
Techsrijan <aswanibtech@gmail.com>
2026-06-09 11:32:51 +0530

    input data and math module

---
+++ find_square_root.py
@@ -0,0 +1,12 @@
+#import math
+#import math as m
+from math import *
+#a,b,c=20,30,40
+'''
+s=(a+b+c)/2
+print("s=",s)
+'''
+#a=math.sqrt(36)
+#a=m.sqrt(36)
+a=sqrt(36)
+print(a)
---
+++ inputdata.py
@@ -0,0 +1,6 @@
+a=input("Enter your name")
+print(a,"type data=",type(a))
+b=int(input("enter a number"))
+print(b)
+c=b+40
+print(c)
